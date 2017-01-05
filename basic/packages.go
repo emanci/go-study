4900a745e9810a1a6d0cf29544a815cbd11056ba
puzhengchao <zhengchaopu@gmail.com>
2017-01-06 00:16:06 +0800

    Update

---
+++ basic/packages.go
@@ -0,0 +1,15 @@
+/**
+ * @author: puzhengchao <zhengchaopu@gmail.com>
+ * @copyright (c) 2017 puzhengchao <zhengchaopu@gmail.com>
+ */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(120))
+}
---
+++ hello.go
@@ -0,0 +1,12 @@
+/**
+ * @author: puzhengchao <zhengchaopu@gmail.com>
+ * @copyright (c) 2017 puzhengchao <zhengchaopu@gmail.com>
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello go, i'm comming!")
+}
